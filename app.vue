b7f07fbb1ae5f54cbc682558ce31003a71f35f1a
firetiger8 <dhananjaya.susanta@gmail.com>
2024-07-08 14:28:59 +0700

    fresh installation nuxt and vuetify

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLoadingIndicator color="#348feb" :height="5" />
+  <NuxtLayout>
+    <v-app>
+      <NuxtWelcome />
+      <!--<NuxtPage />-->
+    </v-app>
+  </NuxtLayout>
+</template>
